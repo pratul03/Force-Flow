72afbc515ed6222735fc45a6722b7e692b823998
Pratul Makar <pratulmakar02@gmail.com>
2026-07-17 02:33:21 +0530

    refactor(payroll): update mark-invoice-paid.dto.ts

--- server/src/modules/payroll/dto/mark-invoice-paid.dto.ts
+++ server/src/modules/payroll/dto/mark-invoice-paid.dto.ts
@@ -1,8 +1,8 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class MarkInvoicePaidDto {
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   actorUserId!: string;
 
   @IsString()
