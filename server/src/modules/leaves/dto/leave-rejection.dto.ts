dbea364515b36a8f06a9df59e8a1f1868c76d616
Pratul Makar <pratulmakar02@gmail.com>
2026-07-17 02:33:17 +0530

    refactor(leaves): update leave-rejection.dto.ts

--- server/src/modules/leaves/dto/leave-rejection.dto.ts
+++ server/src/modules/leaves/dto/leave-rejection.dto.ts
@@ -1,8 +1,9 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class LeaveRejectionDto {
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   actorUserId!: string;
 
   @IsString()
