595f4d64cd5461aea4f14d1b0471f28740f96dc9
Pratul Makar <pratulmakar02@gmail.com>
2026-07-17 02:34:57 +0530

    feat(prisma): add migration.sql

---
+++ server/prisma/migrations/20260716154025_add_department_hierarchy_and_manager/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Department" ADD COLUMN     "managerId" TEXT,
+ADD COLUMN     "parentId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Department" ADD CONSTRAINT "Department_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Department"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Department" ADD CONSTRAINT "Department_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
