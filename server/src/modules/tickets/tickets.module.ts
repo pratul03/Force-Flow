a5862bd5a7e383cba39fa37e620983dcf896366c
Pratul Makar <pratulmakar02@gmail.com>
2026-07-18 02:56:44 +0530

    refactor(tickets): update tickets.module.ts

--- server/src/modules/tickets/tickets.module.ts
+++ server/src/modules/tickets/tickets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './controllers/tickets.controller';
 import { TicketsService } from './services/tickets.service';
+import { TicketsGateway } from './gateways/tickets.gateway';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService],
+  providers: [TicketsService, TicketsGateway],
   exports: [TicketsService],
 })
 export class TicketsModule {}
