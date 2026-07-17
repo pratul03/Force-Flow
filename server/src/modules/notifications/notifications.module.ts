af8819121a6e487c825077f129063a267b874c19
Pratul Makar <pratulmakar02@gmail.com>
2026-07-18 02:56:40 +0530

    refactor(notifications): update notifications.module.ts

--- server/src/modules/notifications/notifications.module.ts
+++ server/src/modules/notifications/notifications.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { NotificationsService } from './services/notifications.service';
+import { MailService } from './services/mail.service';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService],
-  exports: [NotificationsService],
+  providers: [NotificationsService, MailService],
+  exports: [NotificationsService, MailService],
 })
 export class NotificationsModule {}
